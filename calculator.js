59106e1de39557e14726f802eaba29dad71cda95
hamdtc <mohamedtc73@gmail.com>
2023-03-23 13:55:16 +0530

    final

---
+++ calculator.js
@@ -0,0 +1,29 @@
+function calculate() {
+    // get the input values
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var operator = document.getElementById("operator").value;
+    
+    // perform the calculation based on the operator selected
+    var result;
+    switch (operator) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+      default:
+        result = "Invalid operator";
+    }
+    
+    // display the result
+    document.getElementById("result").innerHTML = result;
+  }
+  
